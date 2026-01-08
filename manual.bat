6e2670eeb7c7ec1795ef73cf926047d6ef864c5f
SilkHeaded <silkheaded@proton.me>
2026-01-08 13:12:40 -0500

    Update manual.bat

--- manual.bat
+++ manual.bat
@@ -1,81 +1,54 @@
-@echo off
-color 2
-title manual
-cls
-echo =============================================================================
-echo : WHAT ARE WE? 	
-echo WOWCAT is a batch file simular to command prompt
-echo Created by SilkHeaded and being a small project,
-echo thanks for viewing WOWCAT
-echo =============================================================================
-pause
-cls
-echo =============================================================================
-echo : INFO
-echo WOWCAT is not an actual Windows, MacOS, or Linux software
-echo WOWCAT is not a virus, you may check the source code with [iamparanoid]
-echo WOWCAT may read files only for display and not tracking
-echo WOWCAT is in beta, bugs may occur frequently
-echo WOWCAT uses MIT license as seen here
-echo =============================================================================
-pause
-cls
-echo =============================================================================
-echo : COMMANDS 
-echo [leave] - Exit WOWCAT manually
-echo [help] - Look at the manual
-echo [systeminfo] - Look at system information (SOON)
-echo [iamparanoid] - Look at WOWCAT source code
-echo [viewtemp] - Look at all files created by WOWCAT
-echo [countdown] - Countdown from a numeric value (NOT WORKING)
-echo [brhtml] - Pick up data from DuckDuckGo into a HTML
-echo      It may appear blank when it is run, type there
-echo [clear] - Clear all command history in WOWCAT
-echo [ping] - Test internet or local host connectivity
-echo [tasklist] - Show running processes
-echo [shutdown] - Reboot or power off the computer
-echo [windowsdiskcheck] or [wdc] - Run Windows disk check
-echo [systemfilecheck] or [sfc] - Run system file integrity checker
-echo [textcolor] - Change text
-echo      blue
-echo      green
-echo      aqua
-echo      red
-echo      purple
-echo      yellow
-echo      white
-echo      gray
-echo      light blue
-echo      light green
-echo      light aqua
-echo      light red
-echo      light purple
-echo      light yellow
-echo      bright white
-echo [onscreenkeyboard] or [osk] - Self explained
-echo [calc] or [calculator] - Self explained
-echo [coinflip] - Flip a coin (50/50)
-echo [wp] or [wordpad] - Self explained
-echo [firewall] - Windows Firewall
-echo [mag] - Magnify
-echo [printersfolder] - Open printers folder
-echo [qt] - Open QuickTime
-echo [removablestorage] - Look at removable storage
-echo [saa] or [soundaudio] - Sounds and Audio
-echo [securitysystemtool] or [sst] - Open System Security Tool
-echo [task] - Task Manager
-echo [message] or [msg] - Self explained
-echo       MESSAGE - Message imput
-echo       IP - Device IP to send to, leave blank to leave a pop up 
-echo =============================================================================
-pause
-cls
-echo =============================================================================
-echo : CREDITS
-echo Scriptors: SilkHeaded
-echo Testers: SilkHeaded, BlackStarMythology (Ari), Stardust, fourf3ced
-echo Contributors: You!
-echo =============================================================================
-echo.
-pause
-exit
+
+=========================================================================
+WHAT ARE WE:
+WOWCAT is a batch file simular to command prompt
+Created by SilkHeaded and being a small project,
+Thanks for viewing WOWCAT
+=========================================================================
+
+=========================================================================
+DISCLAIMERS AND INFORMATION:
+WOWCAT is not an actual Windows, MacOS, or Linux software
+WOWCAT is not a virus, you may check the source code with [iamparanoid]
+WOWCAT may read and open files only for display and not tracking
+WOWCAT is in beta, bugs may occur frequently
+WOWCAT uses MIT license as seen here
+=========================================================================
+
+=========================================================================
+
+BSD 3-Clause License
+
+Copyright (c) 2026, SilkHeaded
+
+Redistribution and use in source and binary forms, with or without
+modification, are permitted provided that the following conditions are met:
+
+1. Redistributions of source code must retain the above copyright notice, this
+   list of conditions and the following disclaimer.
+
+2. Redistributions in binary form must reproduce the above copyright notice,
+   this list of conditions and the following disclaimer in the documentation
+   and/or other materials provided with the distribution.
+
+3. Neither the name of the copyright holder nor the names of its
+   contributors may be used to endorse or promote products derived from
+   this software without specific prior written permission.
+
+THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+
+=========================================================================
+CREDITS
+Scriptors: SilkHeaded
+Testers: SilkHeaded, BlackStarMythology (Ari), Stardust, fourf3ced
+Contributors: You!
+=========================================================================
