e80b8e994e705cfaf279e7bc3d0192937ef1f095
SilkHeaded <silkheaded@proton.me>
2026-01-08 07:44:57 -0500

    Update WOWCAT.bat

--- WOWCAT.bat
+++ WOWCAT.bat
@@ -5,7 +5,7 @@ color 0b
 mode con: cols=80 lines=30
 title WOWCAT for Windows 11
 setlocal ENABLEDELAYEDEXPANSION
-set "WOWCAT_VER=2.2"
+set "WOWCAT_VER=2.2.4"
 set "VERSION_URL=https://raw.githubusercontent.com/SilkHeaded/WOWCAT/refs/heads/main/version.txt"
 cls
 echo.
@@ -59,17 +59,63 @@ if "%choice%"=="printerfolder" goto printersfolder
 if "%choice%"=="report" goto report
 if "%choice%"=="notepad" goto notepad
 if "%choice%"=="np" goto notepad
+if "%choice%"=="commands" goto commands
 echo  ERROR 332: Command failed to be recognized, please use [help] or send feedback [report] 
 goto re
 
 :command_
 echo  ERROR 333: Command is valid however not yet programmed, you may become a contributor by [report]
 
-
-:help
-start manual.bat
-goto re
-
+:commands
+echo =============================================================================
+echo : COMMANDS 
+echo [leave] - Exit WOWCAT manually
+echo [help] - Look at the manual
+echo [systeminfo] - Look at system information (SOON)
+echo [iamparanoid] - Look at WOWCAT source code
+echo [viewtemp] - Look at all files created by WOWCAT
+echo [countdown] - Countdown from a numeric value (NOT WORKING)
+echo [brhtml] - Pick up data from DuckDuckGo into a HTML
+echo      It may appear blank when it is run, type there
+echo [clear] - Clear all command history in WOWCAT
+echo [ping] - Test internet or local host connectivity
+echo [tasklist] - Show running processes
+echo [shutdown] - Reboot or power off the computer
+echo [windowsdiskcheck] or [wdc] - Run Windows disk check
+echo [systemfilecheck] or [sfc] - Run system file integrity checker
+echo [textcolor] - Change text
+echo      blue
+echo      green
+echo      aqua
+echo      red
+echo      purple
+echo      yellow
+echo      white
+echo      gray
+echo      light blue
+echo      light green
+echo      light aqua
+echo      light red
+echo      light purple
+echo      light yellow
+echo      bright white
+echo [onscreenkeyboard] or [osk] - Self explained
+echo [calc] or [calculator] - Self explained
+echo [coinflip] - Flip a coin (50/50)
+echo [wp] or [wordpad] - Self explained
+echo [firewall] - Windows Firewall
+echo [mag] - Magnify
+echo [printersfolder] - Open printers folder
+echo [qt] - Open QuickTime
+echo [removablestorage] - Look at removable storage
+echo [saa] or [soundaudio] - Sounds and Audio
+echo [securitysystemtool] or [sst] - Open System Security Tool
+echo [task] - Task Manager
+echo [message] or [msg] - Self explained
+echo       MESSAGE - Message imput
+echo       IP - Device IP to send to, leave blank to leave a pop up 
+echo =============================================================================
+pause goto re
 :report
 start "" "https://forms.cloud.microsoft/r/JZecx1MBtJ"
 echo Thanks for giving feedback^! :]
@@ -135,6 +181,88 @@ echo Listing all active tasks...
 tasklist
 echo.
 goto re
+
+:help
+color 2
+title manual
+cls
+echo =============================================================================
+echo : WHAT ARE WE? 	
+echo WOWCAT is a batch file simular to command prompt
+echo Created by SilkHeaded and being a small project,
+echo thanks for viewing WOWCAT
+echo =============================================================================
+pause
+cls
+echo =============================================================================
+echo : INFO
+echo WOWCAT is not an actual Windows, MacOS, or Linux software
+echo WOWCAT is not a virus, you may check the source code with [iamparanoid]
+echo WOWCAT may read files only for display and not tracking
+echo WOWCAT is in beta, bugs may occur frequently
+echo WOWCAT uses MIT license as seen here
+echo =============================================================================
+pause
+cls
+echo =============================================================================
+echo : COMMANDS 
+echo [leave] - Exit WOWCAT manually
+echo [help] - Look at the manual
+echo [systeminfo] - Look at system information (SOON)
+echo [iamparanoid] - Look at WOWCAT source code
+echo [viewtemp] - Look at all files created by WOWCAT
+echo [countdown] - Countdown from a numeric value (NOT WORKING)
+echo [brhtml] - Pick up data from DuckDuckGo into a HTML
+echo      It may appear blank when it is run, type there
+echo [clear] - Clear all command history in WOWCAT
+echo [ping] - Test internet or local host connectivity
+echo [tasklist] - Show running processes
+echo [shutdown] - Reboot or power off the computer
+echo [windowsdiskcheck] or [wdc] - Run Windows disk check
+echo [systemfilecheck] or [sfc] - Run system file integrity checker
+echo [textcolor] - Change text
+echo      blue
+echo      green
+echo      aqua
+echo      red
+echo      purple
+echo      yellow
+echo      white
+echo      gray
+echo      light blue
+echo      light green
+echo      light aqua
+echo      light red
+echo      light purple
+echo      light yellow
+echo      bright white
+echo [onscreenkeyboard] or [osk] - Self explained
+echo [calc] or [calculator] - Self explained
+echo [coinflip] - Flip a coin (50/50)
+echo [wp] or [wordpad] - Self explained
+echo [firewall] - Windows Firewall
+echo [mag] - Magnify
+echo [printersfolder] - Open printers folder
+echo [qt] - Open QuickTime
+echo [removablestorage] - Look at removable storage
+echo [saa] or [soundaudio] - Sounds and Audio
+echo [securitysystemtool] or [sst] - Open System Security Tool
+echo [task] - Task Manager
+echo [message] or [msg] - Self explained
+echo       MESSAGE - Message imput
+echo       IP - Device IP to send to, leave blank to leave a pop up 
+echo =============================================================================
+pause
+cls
+echo =============================================================================
+echo : CREDITS
+echo Scriptors: SilkHeaded
+echo Testers: SilkHeaded, BlackStarMythology (Ari), Stardust, fourf3ced
+echo Contributors: You!
+echo =============================================================================
+echo.
+pause
+goto me
 
 :shutdown
 echo [1] Shutdown now
@@ -356,6 +484,7 @@ goto :eof
 
 :exit
 exit
+
 
 
 
